0f020a9a31770ab6ee2371c227cebcee8083d624
Edward Solomatin <edwardnero2020@gmail.com>
2022-08-18 12:04:38 +0300

    sfdsf

--- middlewares/cors.middleware.js
+++ middlewares/cors.middleware.js
@@ -1,6 +1,6 @@
 function cors (req, res, next){
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Content-Type")
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
     next();
 }
 
