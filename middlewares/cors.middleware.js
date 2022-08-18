b6ebd278d32fd371048a37f4d9d68764b0012e59
Edward Solomatin <edwardnero2020@gmail.com>
2022-08-18 11:42:26 +0300

    sdfsd

--- middlewares/cors.middleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "https://keypumps-client.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-type");
-    next();
-}
-
