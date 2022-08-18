b1bbc27722ab3573fc0e3bfe210adb0d80767669
Edward Solomatin <edwardnero2020@gmail.com>
2022-08-18 12:31:09 +0300

    sdfsdf

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,13 +3,14 @@ const {jwtSecret} = require('../config');
 
 module.exports = (req, res) => {
     try{
+        console.log(res.headers)
         const token = req.headers.authorization.split(' ')[1];
 
         if(!token){
             return res.status(401).json({error: 'Auth error'});
         }
         const decoded = jwt.verify(token, jwtSecret);
-
+        
         return res.json({user: decoded})
     } catch (err) {
         console.log(err)
